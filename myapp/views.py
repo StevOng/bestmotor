f3eb3acc5f5886aa8a65545a11202460a6bceb7d
Jowie Ardinata <jowieardinata@gmail.com>
2025-03-07 22:21:39 +0700

    membuat autentikasi admin dan sales

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import JsonResponse
+from .models import Admin, Sales
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            admin = Admin.objects.get(username=username)
+            if admin.password == password:
+                request.session['user_type'] = 'admin'
+                request.session['user_id'] = admin.id
+                return redirect('dashboard')
+            else:
+                messages.error(request, "Password salah")
+        except Admin.DoesNotExist:
+            pass
+
+        try:
+            sales = Sales.objects.get(username=username)
+            if sales.password == password:
+                request.session['user_type'] = 'sales'
+                request.session['user_id'] = sales.id
+                return redirect('katalog')
+            else:
+                messages.error(request, "Password salah")
+        except Sales.DoesNotExist:
+            messages.error(request, "Username tidak ditemukan")
+
     return render(request, 'login.html')
 
 def katalog(request):
