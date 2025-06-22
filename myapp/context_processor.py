aa436e4344acb91716bc6c57dc1453ef0179d981
Jowie Ardinata <jowieardinata@gmail.com>
2025-06-22 15:45:30 +0700

    penambahan data notifikasi berupa stok rendah, faktur jatuh tempo dan invoice jatuh tempo serta penambahan scrollable pada notifikasi

--- myapp/context_processor.py
+++ myapp/context_processor.py
@@ -1,3 +1,8 @@
+from django.db.models import F
+from .models.barang import *
+from .models.faktur import *
+from .models.invoice import *
+
 def user_context(request):
     print(request.session.items())
     return {
@@ -5,3 +10,31 @@ def user_context(request):
         'nama': request.session.get('nama'),
         'role': request.session.get('role')
     }
+
+def notifications(request):
+    notifikasi = []
+    low_stocks = Barang.objects.filter(stok__lte=F("stok_minimum")).all()
+    faktur_jto = Faktur.objects.filter(status="jatuh_tempo").all()
+    invoice_jto = Invoice.objects.filter(status="jatuh_tempo").all()
+
+    if low_stocks:
+        for push_notif in low_stocks:
+            notifikasi.append({
+                "head": "New alert from Barang",
+                "msg": f"Barang '{push_notif.nama_barang}' hampir habis sisa '{push_notif.stok}'"
+            })
+    
+    if faktur_jto:
+        for push_notif in faktur_jto:
+            notifikasi.append({
+                "head": "Penagihan faktur sudah bisa diproses",
+                "msg": f"Faktur '{push_notif.no_faktur}' sudah jatuh tempo"
+            })
+
+    if invoice_jto:
+        for push_notif in invoice_jto:
+            notifikasi.append({
+                "head": "Pembayaran invoice sudah bisa diproses",
+                "msg": f"Invoice '{push_notif.no_invoice}' sudah jatuh tempo"
+            })
+    return {"notifikasi": notifikasi}
