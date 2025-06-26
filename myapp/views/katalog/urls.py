3688ed88473c9c5b31640808cdf6caa71e3f6aef
Jowie Ardinata <jowieardinata@gmail.com>
2025-06-27 03:45:44 +0700

    perbaikan tambah katalog dan admin katalog

--- myapp/views/katalog/urls.py
+++ myapp/views/katalog/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .api import KatalogViewSet
+from .api import *
 from . import render
 
 router = DefaultRouter()
 router.register(r'katalog', KatalogViewSet)
+router.register(r'katalogbarang', KatalogBarangViewSet)
 
 urlpatterns = [
     path('admin/', render.admin_katalog, name='admin_katalog'),
     path('tambah/', render.tambah_brgkatalog, name='tambah_brgkatalog'),
+    path('tambah/<int:id>/', render.tambah_brgkatalog, name='tambah_brgkatalog'),
     path('<str:tipe>/', render.katalogbrg, name='katalogbrg'),
     path('<str:tipe>/<int:barang_id>/deskripsi/', render.deskripsi, name='deskripsi'),
 ]
