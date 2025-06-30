b9e7e80c8022b25fbd18df750602ba485e1b1a0d
Jowie Ardinata <jowieardinata@gmail.com>
2025-07-01 06:48:12 +0700

    pilihan sales pada piutang

--- myapp/views/piutang/render.py
+++ myapp/views/piutang/render.py
@@ -3,6 +3,7 @@ from django.db.models import Sum
 from myapp.utils.decorators import admin_required, both_required
 from ...models.piutang import *
 from ...models.faktur import Faktur
+from ...models.user import User
 
 @both_required
 def piutang(request):
@@ -15,10 +16,11 @@ def piutang(request):
 @admin_required
 def tambah_bayarpiutang(request, id=None):
     piutang = None
+    sales = User.objects.all()
     faktur = Faktur.objects.filter(status__in=['belum_lunas', 'jatuh_tempo']).select_related('pesanan_id__customer_id__user_id')
 
     if id:
         piutang = Piutang.objects.select_related('customer_id__user_id').get(id=id)
         piutang.total_faktur = piutang.faktur_set.aggregate(total=Sum('total'))['total'] or 0
         piutang.nilai_byr = PiutangFaktur.objects.filter(piutang=piutang).values_list('nilai_bayar', flat=True)
-    return render(request, 'piutang/tambahpiutang.html', {'data_piutang':piutang, 'data_faktur': faktur})
+    return render(request, 'piutang/tambahpiutang.html', {'data_piutang':piutang, 'data_faktur': faktur, "list_sales": sales})
