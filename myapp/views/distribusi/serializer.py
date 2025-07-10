cca8f0d0227f2de3e6a0e635484c8d99f3fc8337
Steven <stevenongki7@gmail.com>
2025-07-10 22:57:54 +0700

    edit transaksi

--- myapp/views/distribusi/serializer.py
+++ myapp/views/distribusi/serializer.py
@@ -34,14 +34,31 @@ class TransaksiMasukSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         detail_data = validated_data.pop('detail_barang')
         with transaction.atomic():
+            # Simpan perubahan di field TransaksiMasuk
             for attr, value in validated_data.items():
                 setattr(instance, attr, value)
             instance.save()
 
-            TransaksiMasukBarang.objects.filter(transaksi=instance).delete()
+            # Ambil semua detail barang lama untuk rollback stok
+            old_details = TransaksiMasukBarang.objects.filter(transaksi=instance)
+            for detail in old_details:
+                barang = detail.barang
+                barang.stok -= detail.qty  # Rollback stok lama
+                barang.save(update_fields=['stok'])
 
+            # Hapus semua detail barang lama
+            old_details.delete()
+
+            # Buat ulang detail barang baru dan update stok baru
             for item in detail_data:
                 TransaksiMasukBarang.objects.create(transaksi=instance, **item)
+
+                barang_id = item['barang'].id if isinstance(item['barang'], Barang) else item['barang']
+                barang_masuk = item['qty']
+                barang = Barang.objects.get(id=barang_id)
+                barang.stok += barang_masuk
+                barang.save(update_fields=['stok'])
+
         return instance
 
 class TransaksiKeluarBarangSerializer(serializers.ModelSerializer):
@@ -76,12 +93,29 @@ class TransaksiKeluarSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         detail_data = validated_data.pop('detail_barang')
         with transaction.atomic():
+            # Simpan perubahan di field TransaksiMasuk
             for attr, value in validated_data.items():
                 setattr(instance, attr, value)
             instance.save()
 
-            TransaksiKeluarBarang.objects.filter(transaksi=instance).delete()
+            # Ambil semua detail barang lama untuk rollback stok
+            old_details = TransaksiKeluarBarang.objects.filter(transaksi=instance)
+            for detail in old_details:
+                barang = detail.barang
+                barang.stok += detail.qty  # Rollback stok lama
+                barang.save(update_fields=['stok'])
 
+            # Hapus semua detail barang lama
+            old_details.delete()
+
+            # Buat ulang detail barang baru dan update stok baru
             for item in detail_data:
                 TransaksiKeluarBarang.objects.create(transaksi=instance, **item)
+
+                barang_id = item['barang'].id if isinstance(item['barang'], Barang) else item['barang']
+                barang_keluar = item['qty']
+                barang = Barang.objects.get(id=barang_id)
+                barang.stok -= barang_keluar
+                barang.save(update_fields=['stok'])
+
         return instance
